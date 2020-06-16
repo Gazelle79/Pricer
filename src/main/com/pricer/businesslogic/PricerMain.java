f88313b11dbdac04dc8f413e944ae1354ff97cac
Frederick Boyd Jr <fboydjr@gmail.com>
2020-06-16 10:20:40 -0400

    Committing.

--- src/main/com/pricer/businesslogic/PricerMain.java
+++ src/main/com/pricer/businesslogic/PricerMain.java
@@ -10,7 +10,6 @@ public class PricerMain {
         String currentPath = new File("").getAbsolutePath();
         int targetSize = 200;
 
-        //String inputFileNameAndPath =  currentPath + "/InputFiles/PricerInput.txt";
         String inputFileNameAndPath =  currentPath + "/InputFiles/PricerInput2.txt";
         //String inputFileNameAndPath =  currentPath + "/InputFiles/pricer.in";
         //String outputFileNameAndPath = currentPath + "/OutputFiles/PricerOutput.txt";
@@ -30,7 +29,5 @@ public class PricerMain {
         {
             System.err.println("Something went wrong: " + e.toString());
         }
-
-
     }
 }
