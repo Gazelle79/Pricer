7fcc48f5f54f71b2b90a4435f4033e6421d2cc16
Frederick Boyd Jr <fboydjr@gmail.com>
2020-06-21 15:23:55 -0400

    Wrapping up. Changed comments in ReadMe. Minor changes to unit tests. Renamed 'PricerMain' to 'Pricer'. COmmitting.

--- src/main/com/pricer/businesslogic/Pricer.java
+++ src/main/com/pricer/businesslogic/Pricer.java
@@ -2,26 +2,26 @@ package main.com.pricer.businesslogic;
 
 import java.io.File;
 
-public class PricerMain {
+public class Pricer {
 
     public static void main(String[] args)
     {
-        String pricerDataInfo = "";
         String currentPath = new File("").getAbsolutePath();
         int targetSize = 200;
 
         String inputFileNameAndPath =  currentPath + "/InputFiles/PricerInput2.txt";
         //String inputFileNameAndPath =  currentPath + "/InputFiles/pricer.in";
 
-        if(args.length > 1)
-        {
-            targetSize = Integer.parseInt(args[0]);
-        }
-
-        //Get *all* market data. Read it in & display it.
-        OrderBook shareOrderBook = new OrderBook(targetSize);
         try
         {
+            if(args.length >= 1)
+            {
+                targetSize = Integer.parseInt(args[0]);
+            }
+            //else, targetSize = 200 by default
+
+            //Get *all* market data. Read it in & display it.
+            OrderBook shareOrderBook = new OrderBook(targetSize);
             shareOrderBook.readFinanceData(inputFileNameAndPath);
         }
         catch(Exception e)
